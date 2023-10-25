7474ab14cabad62f0703a99b5f98ddce0e2fde88
noahkise <noah@kisefamily.com>
2023-10-25 14:29:20 -0700

    pass test 1

---
+++ pig-latin/js/scripts.js
@@ -0,0 +1,11 @@
+function pigLatin(word) {
+    let result = word + "way";
+    return result;
+}
+
+
+// Test: "It will add 'way' to the end of words that begin with a vowel."
+
+// Test: "For words beginning with 1 consonant it will move the first letter to the end."
+
+// Test: "For words beginning with 1 consonant it will move the first letter to the end and add 'ay'."
